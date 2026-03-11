ec3754aa7833c5dc56e368dc8f7d169c087cc9f5
PostCalc <chaitanpatil96k@gmail.com>
2026-03-11 22:11:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// ✅ Version v31 (Silent Auto-Update Strategy)
-const CACHE_NAME = "postcalc-cache-v31";
+// ✅ Version v32 (Silent Auto-Update Strategy)
+const CACHE_NAME = "postcalc-cache-v32";
 
 const PRECACHE_ASSETS = [
   "./",
